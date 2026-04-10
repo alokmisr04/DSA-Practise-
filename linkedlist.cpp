a0f567cf5c11e51d3fd3dacd0dd467470c7686eb
alokmisr04 <alokmisr04@gmail.com>
2026-04-10 15:33:24 +0530

    linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,7 @@
+Linked list is a linera data structure.Linked list store data in non contigous manner.Linked list has various nodes and each nodes contains a value and a next part.
+The value is used to store the value of the currnet node and the next is used to store the address of the next node .The first node in each linked list is known as 
+a head and with the helo of the head we can traverse to th whole linked list.Unlike the array :
+                            The linked list uses dynamic memory allocation so the memory is assigned during the runtime . 
+                            The operation like insertion an ddeleltion in linked list is very efficient as compared to array the insertion/deletion
+                            operation takes only O(1) constanst time in comparison of array which takes O(n) times.
+                            The linked list use non contigous memory allocation that means it can be stored anywhere in the memory even if contigous space is not avilable.

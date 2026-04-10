7394d117bdf7e88511ce2200be9bd355c0e095fb
alokmisr04 <alokmisr04@gmail.com>
2026-04-10 15:35:48 +0530

    Create insertionatthebegining.cpp

---
+++ insertionatthebegining.cpp
@@ -0,0 +1,35 @@
+//Inserting elemnt at the beginging /or in the position of head.
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* next;
+};
+void insertAtHead(Node* &head, int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
+}
+void display(Node* head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+int main() {
+    Node* head = NULL;
+    int n, value;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cout << "Enter value " << i+1 << ": ";
+        cin >> value;
+        insertAtHead(head, value);
+    }
+    cout << "\nLinked List: ";
+    display(head);
+    return 0;
+}
